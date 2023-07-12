0217a03188633022daafc55f306ffc34ab859e0a
Joel Linder <joelsc.linder@gmail.com>
2023-07-12 17:38:26 +0200

    Added LCDC variable

--- SoulyaBoy.Core/SBGpu.fs
+++ SoulyaBoy.Core/SBGpu.fs
@@ -2,9 +2,11 @@ namespace SoulyaBoy.Core
 
 type SBGpu = {
     LY: byte
+    LCDC: byte
 }
 
 module SBGpuFactory = 
     let CreateGPU = 
-        {LY = 0uy}
+        { LY = 0uy 
+          LCDC = 0x91uy }
 
