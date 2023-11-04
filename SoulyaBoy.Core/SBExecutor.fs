905bfb5bda723527f1daf9cbd62ee0d727bdd2f6
Joel Linder <joelsc.linder@gmail.com>
2023-11-04 21:47:59 +0100

    V-Blank interrupt, quicker rendering and opcode fixes

--- SoulyaBoy.Core/SBExecutor.fs
+++ SoulyaBoy.Core/SBExecutor.fs
@@ -4,25 +4,53 @@ module internal SBExecutor =
 
     let private sb = new SBBuilder()
 
+    let private INTERRUPT_ADDRESSES = Map([
+        (0, 0x0040us)
+    ])
+
     let private IncrementPC pcd = sb {
         let! mb = SB.Get
         do! SB.Put { mb with CPU = { mb.CPU with PC = mb.CPU.PC + 1us + pcd } } 
     }
 
-    let private handleInterruptState cycles = sb {
+    let private UpdateIMEFlag = sb {
         let! mb = SB.Get
 
-        let handledMB = match mb.CPU.Interrupt with
-                         | Disable -> { mb with CPU = { mb.CPU with Interrupt = Disabling } }
-                         | Disabling -> { mb with CPU = { mb.CPU with Interrupt = Disabled } }
-                         | Enable -> { mb with CPU = { mb.CPU with Interrupt = Enabling } }
-                         | Enabling -> { mb with CPU = { mb.CPU with Interrupt = Enabled } }
+        let handledMB = match mb.CPU.IME with
+                         | Disable -> { mb with CPU = { mb.CPU with IME = Disabling } }
+                         | Disabling -> { mb with CPU = { mb.CPU with IME = Disabled } }
+                         | Enable -> { mb with CPU = { mb.CPU with IME = Enabling } }
+                         | Enabling -> { mb with CPU = { mb.CPU with IME = Enabled } }
                          | _ -> mb
 
         do! SB.Put handledMB
     }
 
+    let rec private TryRunInterrupt i IE IF = sb {
+        if int IE >>> i <> 0 && int IF >>> i <> 0 then
+            let! mb = SB.Get
+            do! SB.Put {mb with CPU = { mb.CPU with IF = IF &&& ~~~(1uy<<<i) }}       
+
+            do! SBOpcodes.Calls.CALL INTERRUPT_ADDRESSES[i]
+            printf "Executing interrupt at address %X\n" INTERRUPT_ADDRESSES[i]
+        elif i < 4 then
+            do! TryRunInterrupt (i+1) IE IF
+    }
+
+    let private HandleInterrupts = sb {
+        let! mb = SB.Get
+
+        let IE = mb.CPU.IE
+        let IF = mb.CPU.IF
+
+        if(mb.CPU.IME = Enabled) && IE <> 0uy && IF <> 0uy then
+            do! SB.Put { mb with CPU = { mb.CPU with IME = Disabled }}
+            do! TryRunInterrupt 0 IE IF
+    }
+
     let private RetrieveOpcodeInstruction opcode (instructions: SBInstructionTable) = sb {
+        printf "%X\n" opcode
+        
         if instructions.ContainsKey(opcode) then
             return instructions[opcode]
         else 
@@ -76,13 +104,16 @@ module internal SBExecutor =
         let! stop = GetStop
 
         if not stop then 
-            do! SBGraphics.Process pixelPipe
+            do! UpdateIMEFlag
+            do! HandleInterrupts
+
             let! instruction = FetchInstruction
-            let! (operation, _, pcd, cycles) = ResolveOperation instruction
-        
+            let! (operation, _, pcd, _) = ResolveOperation instruction
+
             do! IncrementPC pcd
             do! operation
-            do! handleInterruptState cycles
 
+            do! SBGraphics.Process pixelPipe
+            
         return! SB.Get
     }
